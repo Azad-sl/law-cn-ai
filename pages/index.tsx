2ac105997ec6695520ba89031bbf4d8e3ce9f151
Azad-sl <85327306+Azad-sl@users.noreply.github.com>
2023-05-07 15:39:58 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>AI 法律助手</title>
+        <title>AI 法律助手 - 基于ChatGPT</title>
         <meta
           name="description"
-          content="AI 法律助手"
+          content="AI 法律助手 - 基于ChatGPT"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -41,16 +41,16 @@ export default function Home() {
         </div>
 
         <div className='mt-28 md:mt-40 text-center w-full'>
-          <h2 className='text-slate-500'>更多好玩</h2>
+          <h2 className='text-slate-500'>更多推荐</h2>
           <ul className='grid grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 mt-4 max-w-lg mx-auto text-xs'>
             <li>
-              <Link href="https://magickpen.com/?ref=lawcnai" className=' 
+              <Link href="https://chatbot.aihub.ren/zh" className=' 
               dark:bg-white/30
               dark:text-slate-900
               dark:border-slate-900
                text-slate-400  border border-slate-300/30 transition-all hover:bg-white/50 hover:backdrop-blur-md py-2.5 rounded-md block'>
-                <Image src={'/MagickPen.png'}  width={100} height="20" className='w-full mb-1 h-5 object-contain' alt="MagickPen - 智能写作助手" />
-                智能写作助手
+                <Image src={'/chatbot.png'}  width={100} height="20" className='w-full mb-1 h-5 object-contain' alt="chatbot - ChatGPT镜像" />
+                ChatGPT镜像
               </Link>
             </li>
             <li>
